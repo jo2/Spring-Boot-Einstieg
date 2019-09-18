0d2383ad67da50d7a0f64df7d7bc1271ad4f37b2
Johannes Teklote <johannes.teklote@web.de>
2019-09-18 17:35:32 +0200

    Task 1

---
+++ src/main/java/de/adesso/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
