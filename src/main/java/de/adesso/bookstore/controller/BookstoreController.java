0b9ccfc097c9663346c82d26b98a7f0beaf08dfd
Johannes Teklote <johannes.teklote@adesso.de>
2021-07-20 11:30:05 +0200

    add api doc

--- src/main/java/de/adesso/bookstore/controller/BookstoreController.java
+++ src/main/java/de/adesso/bookstore/controller/BookstoreController.java
@@ -2,20 +2,20 @@ package de.adesso.bookstore.controller;
 
 import de.adesso.bookstore.entitites.Book;
 import de.adesso.bookstore.services.BookstoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class BookstoreController {
 
     private final BookstoreService bookstoreService;
@@ -25,31 +25,67 @@ public class BookstoreController {
         this.bookstoreService = bookstoreService;
     }
 
+    @Operation(summary = "Get all books.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Book.class))) })
+    })
     @GetMapping("/books")
     public List<Book> getAllBooks() {
         return bookstoreService.getAllBooks();
     }
 
+    @Operation(summary = "Get a book by its title.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) })
+    })
     @GetMapping("/books/title/{title}")
     public Book getBookByTitle(@PathVariable(name = "title") String title) {
         return bookstoreService.getBookByTitle(title);
     }
 
+    @Operation(summary = "Get a book by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) })
+    })
     @GetMapping("/books/{id}")
-    public Book getBookByTitle(@PathVariable(name = "id") Long id) {
+    public Book getBookById(@PathVariable(name = "id") Long id) {
         return bookstoreService.findById(id);
     }
 
+    @Operation(summary = "Add a book.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Added the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) })
+    })
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
         return bookstoreService.addBook(book);
     }
 
+    @Operation(summary = "Update a book.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated the book.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) })
+    })
     @PutMapping("/books/{id}")
     public Book updateBook(@PathVariable(name = "id") Long id, @RequestBody Book book) {
         return bookstoreService.updateBook(id, book);
     }
 
+    @Operation(summary = "Delete a book.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted the book.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) })
+    })
     @DeleteMapping("/books/{id}")
     public void deleteBook(@PathVariable(name = "id") Long id) {
         bookstoreService.removeBook(id);
